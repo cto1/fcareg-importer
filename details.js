a475968e020eaee5b2b0bf9345fed0671d10360a
cto1 <a_kudic@yahoo.co.uk>
2016-09-06 00:03:37 +0100

    single bunddle

--- details.js
+++ details.js
@@ -21,22 +21,24 @@ var url = 'https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAA
 
 var file = 'details.json';
 
-x('https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAAF', '#content', {
-  name: 'h1',
+x(url, '#content', {
+  name: '.RecordName',
+  
   pervious_names: '.FirmPreviousNames | trim | replace_new_line | replace_space',
   status: '.alert h4 .statusbox | trim | replace_new_line | replace_space',
   ref_number: '.ReferenceNumber | trim | replace_new_line | replace_space',
   products_covered: x('.ProductsCovered', {
-    products: ['li']}),
+    products: ['li  | trim | replace_new_line | replace_space']}),
   other_note: x('.alert span+ span', {
     title: 'h4', 
-    note: ['p']}),
+    note: ['p | trim | replace_new_line | replace_space']}),
   details: x('https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAAF', '.panel-group .panel', [{
   headings: '.panel-heading .panel-title | trim | replace_new_line | replace_space',
   body: '.panel-body | trim | replace_new_line | replace_space'
   }])  
 })//(function(err, obj) {console.log(err); console.log(obj);})
 .write(file)
+
 /*
 x('https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAAF', '.panel-group .panel', [{
   headings: '.panel-heading .panel-title | trim | replace_new_line | replace_space',
@@ -45,6 +47,6 @@ x('https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAAF', '.pa
 }])//(function(err, obj) {console.log(err); console.log(obj);})
 .write(file)
 
+  */
   var end = new Date().getTime();
   console.log ('Run time: ' + (end - start) + 'ms')
-  */
