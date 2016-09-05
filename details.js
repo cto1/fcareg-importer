e2e437beb9021009fa73fe0466fb7d527129b27c
cto1 <a_kudic@yahoo.co.uk>
2016-09-05 23:36:38 +0100

    details working need to clean \n and remove individuals

--- details.js
+++ details.js
@@ -1,33 +1,50 @@
 var Xray = require('x-ray');
-var x = Xray();
+var x = Xray({
+  filters: {
+    trim: function (value) {
+      return typeof value === 'string' ? value.trim() : value
+    },
+    replace_space: function (value) {
+      return typeof value === 'string' ? value.replace(/ +(?= )/g,'') : value
+    },
+    replace_new_line: function (value) {
+      return typeof value === 'string' ? value.replace(/\n/g,' ') : value
+    },
+    remove_individuals: function (value) {
+      return typeof value === 'string' && value.indexOf('individual') !== -1 ? '' : value
+    }
+  }
+});
 
-var start = new Date();
+var start = new Date().getTime();
 var url = 'https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAAF';
 
 var file = 'details.json';
 
-//x('https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAAF',  {
- //  firm: 'title',
-//  fca_link: ['a@href'],
-//  trading_names: ['.TradingNames'],
-//  type_of_business: ['.search_popover'],
-//  ref_number: ['tr td+ td+ td+ td'],
-//  status: ['.CurrentStatus'],
-  //count: +i,
-// })(function(err, obj) {console.log(err); console.log(obj);})
-// .write(file)
-
-
 x('https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAAF', '#content', {
   name: 'h1',
-  pervious_names: '.FirmPreviousNames',
-  status: '.alert h4 .statusbox',
-  ref_number: '.ReferenceNumber',
+  pervious_names: '.FirmPreviousNames | trim | replace_new_line | replace_space',
+  status: '.alert h4 .statusbox | trim | replace_new_line | replace_space',
+  ref_number: '.ReferenceNumber | trim | replace_new_line | replace_space',
   products_covered: x('.ProductsCovered', {
-    products: ['li']})
+    products: ['li']}),
+  other_note: x('.alert span+ span', {
+    title: 'h4', 
+    note: ['p']}),
+  details: x('https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAAF', '.panel-group .panel', [{
+  headings: '.panel-heading .panel-title | trim | replace_new_line | replace_space',
+  body: '.panel-body | trim | replace_new_line | replace_space'
+  }])  
 })//(function(err, obj) {console.log(err); console.log(obj);})
 .write(file)
+/*
+x('https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000000MfIeqAAF', '.panel-group .panel', [{
+  headings: '.panel-heading .panel-title | trim | replace_new_line | replace_space',
+  body: '.panel-body | trim | replace_new_line | replace_space'
+  //basic_details: '.panel .panel',
+}])//(function(err, obj) {console.log(err); console.log(obj);})
+.write(file)
 
-
-  var end = new Date();
-  console.log ('Run time: ' + (end.getMilliseconds() - start.getMilliseconds()) + 'ms')
+  var end = new Date().getTime();
+  console.log ('Run time: ' + (end - start) + 'ms')
+  */
