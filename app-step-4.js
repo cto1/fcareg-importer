42fa28c337b7ea1f4b9f439b2b38e91403c44de3
cto1 <a.kudic@gmail.com>
2016-12-09 21:08:52 +0000

    Sync request example and clean up of step 4 - still async

--- app-step-4.js
+++ app-step-4.js
@@ -1,20 +1,31 @@
 const fs = require('fs');
 const request = require('request');
 
+function log(source, message) {
+  var now = new Date().toString();
+  var log = `${source}-${message} ${now}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+};
+
 var filesArray = fs.readdirSync ('./3_firm_links/todo')
 console.log(filesArray);
 
 for (f in filesArray) {
-  console.log('START----------', filesArray[f]);
+  log(filesArray[f],'------------START----------');
   //get serach array
   var searchArray = fs.readFileSync(`./3_firm_links/todo/${filesArray[f]}`);
   var linksArray = JSON.parse(searchArray);
 
   for (i in linksArray) {
     var searchUrl = linksArray[i];
-    var saveFile = `./4_firm_links_search/${linksArray[i].slice(-18)}.html`;
-    //console.log('Url ', searchUrl, ' in file ', saveFile);
-    request(searchUrl).pipe(fs.createWriteStream(saveFile));
+    var fileRef = `${filesArray[f]}-${i}`;
+    var saveFile = `./4_firm_links_search/${fileRef}.html`;
+    request(searchUrl)
+    .on('response', (response) => log(fileRef, `Response: ${response.statusCode}`))
+    .pipe(fs.createWriteStream(saveFile))
+    .on('error', (error) => log(fileRef, `Error: ${error}`))
+    .on('finish', () => log(fileRef, 'Finish'));
   }
-  console.log('END ', filesArray[f], 'procesed ', i);
+  log(filesArray[f], `END of file. Processed: ${i+1}`);
 }
