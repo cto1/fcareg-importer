aaaaf07f2eb1bb916a4afc14bab6f614ed75a2b1
cto1 <Aleks.Kudic@grove-cm.com>
2016-12-09 12:10:42 +0000

    Links chunked to 10

--- app-step-3.js
+++ app-step-3.js
@@ -6,14 +6,12 @@ var createSmallerArrayFiles = function(arr, chunkSize) {
     var smallArray, i;
     for (i = 0; i < arr.length; i += chunkSize) {
         smallArray = arr.slice(i, i + chunkSize);
-        fs.writeFileSync(`./3_firm-links/firm-links${i}.json`, JSON.stringify(smallArray))
-        chunkSize = Math.floor(Math.random()*(110-25+1)+25);
-        console.log(chunkSize);
+        fs.writeFileSync(`./3_firm_links/${i}-${i-1+chunkSize}.json`, JSON.stringify(smallArray))
     }
     return smallArray;
 }
 
-var lastSmallArray = createSmallerArrayFiles(linksArray, 50);
+var lastSmallArray = createSmallerArrayFiles(linksArray, 10);
 var result = JSON.stringify(lastSmallArray);
 
 console.log(result);
